c96bc49a719012b5ec4610ea4eeea02c444ec0ee
taonas <c13329501@mydit.ie>
2017-03-21 19:16:20 +0000

    Weather client side

--- client.js
+++ client.js
@@ -9,10 +9,11 @@ socket.on('welcome', function(data) {
     // Respond with a message including this clients' id sent from the server
     socket.emit('i am client', {data: 'foo!', id: data.id});
 });
-socket.on('time', function(data) {
-    console.log(data.message);
-    addMessage(data.message.temp);
+socket.on('weather', function(data) {
+    weather(data.message);
+    // addMessage(data.message.temp);
 });
+
 socket.on('error', console.error.bind(console));
 socket.on('message', console.log.bind(console));
 
@@ -24,4 +25,41 @@ function addMessage(message) {
 
     el.appendChild(text);
     location.appendChild(el);
+}
+
+
+function weather(data){
+
+    if(document.getElementById("temperature") === null)
+    {
+        var wrapper = document.createElement("div");
+
+        var large = document.createElement("div");
+        large.className = "large light";
+
+        // var weatherIcon = document.createElement("span");
+        // weatherIcon.className = "wi weathericon " + this.weatherType;
+        // large.appendChild(weatherIcon);
+
+        var temperature = document.createElement("span");
+        temperature.className = "bright";
+        temperature.id = "temperature";
+        temperature.innerHTML = " " + roundValue(data.temp) + "&deg;";
+        large.appendChild(temperature);
+
+        wrapper.appendChild(large);
+        document.getElementById("weather").appendChild(wrapper);
+    }
+    else
+    {
+        var temperature = document.getElementById("temperature");
+        temperature.innerHTML = " " + roundValue(data.temp) + "&deg;";
+    }
+
+    return wrapper;
+}
+
+function roundValue(temperature) {
+    const decimals = 0;
+    return parseFloat(temperature).toFixed(decimals);
 }
--- server.js
+++ server.js
@@ -27,60 +27,27 @@ const port = 9745;
 const server = https.createServer(options, app);
 const io = require('socket.io')(server);
 
+
+//WEATHER
 const weather = require("Openweather-Node");
 weather.setAPPID("fc95ca7a3fa739623373a2c2a9fe1198");
 weather.setCulture("ie");
 weather.setForecastType("daily");
 
-
 const currentWeather = function() {
-
+    console.log("startedwather")
     try{
-        weather.now("Dublin", function (err, aData) {
-
-            console.log(aData.getDegreeTemp());
-            io.emit('time', { message: aData.getDegreeTemp()});
-
-            // if (err) console.log(err);
-            // else {
-            //     this.temp = null;
-            //
-            //     var data = aData.getDegreeTemp();
-            //
-            //     console.log(roundValue(data.temp) + " Degrees");
-            //     console.log(data.temp_min);
-            //     console.log(data.temp_max);
-            //     console.log(data);
-            //
-            //     return data;
-            //     // console.log(temp);
-            // }
+        weather.now("DublinCity", function (err, aData) {
+            io.emit('weather', { message: aData.getDegreeTemp()});
         });
     }
     catch(err) {
         console.log(err.message);
         return 1;
     }
-
 }
-
-function roundValue(temperature) {
-    const decimals = 0;
-    return parseFloat(temperature).toFixed(decimals);
-}
-
-
-
-
-
-
-
 
 setInterval(currentWeather, 1000);
-
-
-
-
 
 
 
@@ -108,24 +75,16 @@ app.get('/parse-m3u', (req, res) => {
     }
 
     const urls = [];
-
     request(m3uUrl, function(error, response, bodyResponse) {
         console.log(bodyResponse, m3uUrl);
         if (bodyResponse) {
             urls.push(bodyResponse);
         }
-
         res.json(urls);
     });
 });
 
-function sendTime() {
-    io.emit('time', { message: currentWeather()});
-}
 
-
-// Send current time every 10 secs
-//setInterval(sendTime, 1000);
 
 // Emit welcome message on connection
 io.on('connection', function(socket) {
@@ -138,7 +97,7 @@ io.on('connection', function(socket) {
 
 
 
-server.listen(port, function() {
+server.listen(port, "0.0.0.0", function() {
     console.log('server up and running at %s port', port);
 });
 
